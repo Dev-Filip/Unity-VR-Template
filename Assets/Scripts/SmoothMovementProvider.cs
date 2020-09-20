31f3e63f6ebdb72f58fcaf2ed3c67b03de73011b
Tomas Utaravicius <tomasutaravicius@gmail.com>
2020-09-20 16:50:18 +0200

    Session6End

---
+++ Assets/Scripts/SmoothMovementProvider.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+public class SmoothMovementProvider : LocomotionProvider
+{
+    //Variable for speed
+    [SerializeField]private float speed = 1.0f;
+    //Variable for the controller
+    public XRController controller = null;
+    //Variable for character controller
+    private CharacterController characterController;
+    //Variable for head
+    public GameObject head;
+
+    protected override void Awake()
+    {
+        //Call to base function
+        base.Awake();
+        //Configure character controller
+        characterController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        //Move the character controller
+        MoveCharacter();
+        //Read the input
+        CheckForInput();
+    }
+    private void MoveCharacter()
+    {
+        //Get head y position apply it to the character controller
+        float headHeight = Mathf.Clamp(head.transform.localPosition.y, 1f, 2f);
+        characterController.height = headHeight;
+
+
+        Vector3 newCenter = new Vector3(0f, 0f, 0f);
+        //Cut in half to find center height    
+        newCenter.y = characterController.height / 2;
+
+        //Get head position on x and z axis apply to the center
+        newCenter.x = head.transform.localPosition.x;
+        newCenter.z = head.transform.localPosition.z;
+
+        //Apply final head position to the character controller center
+        characterController.center = newCenter;
+
+    }
+    private void CheckForInput()
+    {
+        if(controller.enableInputActions)
+        {
+            if(controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 position))
+            {
+                StartMove(position);
+            }
+        }
+    }
+    private void StartMove(Vector2 analogPosition)
+    {
+        //Convert analog position from Vector2 to Vector3
+        Vector3 joystickDirection = new Vector3(analogPosition.x, 0f, analogPosition.y);
+        //Convert head rotation into vector3
+        Vector3 headRotation = new Vector3(0f, head.transform.eulerAngles.y, 0f);
+        //Compute a common direction from head and analog input
+        Vector3 direction = Quaternion.Euler(headRotation) * joystickDirection;
+        //Multiply computed common direction with speed
+        Vector3 movement = direction * speed;
+        //Apply it to the character controller
+        characterController.Move(movement*Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TriggerButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TriggerButton : XRBaseInteractable
+{
+    //Variables for highest and lowest button point
+    private float yMax = 0f;
+    private float yMin = 0f;
+
+    //Variable for maxDepth
+    [SerializeField] private float maxDepth = 0.01f;
+
+    //Variable for Unity Events OnPress/OnRelease
+    public UnityEvent OnPress = null;
+    public UnityEvent OnRelease = null;
+
+    protected override void Awake()
+    {
+        //Call base class function
+        base.Awake();
+
+        //Configure button highest and lowest point variables
+        yMax = transform.localPosition.y;
+        yMin = transform.localPosition.y - maxDepth;
+    }
+
+    protected override void OnSelectEnter(XRBaseInteractor interactor)
+    {
+        //Call base class function
+        base.OnSelectEnter(interactor);
+        //Update button position (it goes down)
+        SetYPosition(yMin);
+        //Fire event OnPress
+        OnPress.Invoke();
+    }
+    protected override void OnSelectExit(XRBaseInteractor interactor)
+    {
+        //Call base class function
+        base.OnSelectExit(interactor);
+        //Update button position (it goes up)
+        SetYPosition(yMax);
+        //Fire event OnRelease
+        OnRelease.Invoke();
+    }
+    private void SetYPosition(float position)
+    {
+        Vector3 newPosition = transform.localPosition;
+        newPosition.y = position;
+        transform.localPosition = newPosition;
+    }
+}
