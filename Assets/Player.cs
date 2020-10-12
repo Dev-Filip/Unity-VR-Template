dc62d26076baece55624c0aa2ff816baa5befa93
Tomas Utaravicius <tomasutaravicius@gmail.com>
2020-10-12 15:02:54 +0200

    Mirror networking setup, lobby system essentials

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] private Vector3 movement = new Vector3();
+
+    [Client]
+    private void Update()
+    {
+        if (!hasAuthority) { return; }
+
+
+        if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+
+        transform.Translate(movement);
+        //CmdMove();
+    }
+    [Command]
+    private void CmdMove()
+    {
+        transform.Translate(movement);
+
+    }
+    [ClientRpc]
+    private void RpcMove()
+    {
+        transform.Translate(movement);
+    }
+}
---
+++ Assets/Scripts/UI/JoinLobbyMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinLobbyMenu : MonoBehaviour
+{
+    [SerializeField] private VRNetworkManager networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+    [SerializeField] private TMP_InputField ipAddressInputField = null;
+    [SerializeField] private Button joinButton = null;
+
+      private void OnEnable()
+        {
+            VRNetworkManager.OnClientConnected += HandleClientConnected;
+            VRNetworkManager.OnClientDisconnected += HandleClientDisconnected;
+        }
+
+        private void OnDisable()
+        {
+            VRNetworkManager.OnClientConnected -= HandleClientConnected;
+            VRNetworkManager.OnClientDisconnected -= HandleClientDisconnected;
+        }
+
+        public void JoinLobby()
+        {
+            string ipAddress = ipAddressInputField.text;
+
+            networkManager.networkAddress = ipAddress;
+            networkManager.StartClient();
+
+            joinButton.interactable = false;
+        }
+
+        private void HandleClientConnected()
+        {
+            joinButton.interactable = true;
+
+            gameObject.SetActive(false);
+            landingPagePanel.SetActive(false);
+        }
+
+        private void HandleClientDisconnected()
+        {
+            joinButton.interactable = true;
+        }
+}
